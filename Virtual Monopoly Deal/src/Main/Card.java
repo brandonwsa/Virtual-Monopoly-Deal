3bf2aa159ae9e971a8de07b827be700d9635a208
becca <becca@DESKTOP-49JID5K.lan>
2020-10-04 14:47:10 -0500

    Added basic infrastructure

--- Virtual Monopoly Deal/src/Main/Card.java
+++ Virtual Monopoly Deal/src/Main/Card.java
@@ -1,1 +1,32 @@
 /* Card.Java */
+public class Card{
+    
+    private String name;
+    private String type;
+    private int value;
+    private String color_1;
+    private String color_2;
+    
+    public void createCard(String cardName){
+        name = cardName;
+}
+    public String getName(){
+        return name;
+   }
+    
+    public String getType(){
+        return type;
+    }
+    
+    public int getValue(){
+        return value;
+    }
+    
+    public String getColor_1(){
+        return color_1;
+    }
+    
+    public String getColor_2(){
+        return color_2;
+    }
+}
