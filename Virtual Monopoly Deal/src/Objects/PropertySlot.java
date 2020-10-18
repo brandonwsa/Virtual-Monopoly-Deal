fe7d532900199f96a4abd3a15fb14d8d78b18d4b
becca <becca@DESKTOP-DFBK0M4.lan>
2020-10-18 16:37:43 -0500

    Updated PlayScreen Layout

--- Virtual Monopoly Deal/src/Objects/PropertySlot.java
+++ Virtual Monopoly Deal/src/Objects/PropertySlot.java
@@ -1,3 +1,28 @@
 package Objects;
 
+import java.util.ArrayList;
 
+public class PropertySlot{
+    private ArrayList<Card> propertiesInSlot;
+    private int rentAmount;
+    private int amountOwned;
+    private int propertySlotNumber;
+    
+    public void createPropertySlot(int create){}
+    
+    public int addPropertyCard(Card cardAdd){return 0;}
+    
+    public int getRentAmount(){return rentAmount;}
+    
+    public int getAmountOwned(){return amountOwned;}
+    
+    public int getPropertySlotNumber(){return propertySlotNumber;}
+    
+    public ArrayList<Card> getPropertiesInSlot(){return propertiesInSlot;}
+    
+    
+        
+
+    
+
+}
